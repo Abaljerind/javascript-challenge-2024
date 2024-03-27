c21cb8cf54047d2321bcb8511d96702973fafa1a
Abaljerind <abaljerinda@gmail.com>
2024-03-27 09:23:29 +0700

    day 5/366

---
+++ 5-convert-boolean-value-to-strings.js
@@ -0,0 +1,15 @@
+// Codewars Challenge 2024
+// 5/366
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+
+function boolToWord(bool) {
+  //   if (bool == true) {
+  //     return "Yes";
+  //   } else {
+  //     return "No";
+  //   }
+
+  return bool ? "Yes" : "No";
+}
+
+console.log(boolToWord(true));
