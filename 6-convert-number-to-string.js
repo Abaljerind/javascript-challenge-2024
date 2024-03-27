938c2b7466d212b9da37b718fa4908aa5cb21010
Abaljerind <abaljerinda@gmail.com>
2024-03-27 09:28:30 +0700

    day 6/366

---
+++ 6-convert-number-to-string.js
@@ -0,0 +1,12 @@
+// Codewars Challenge 2024
+// 6/366
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+
+// function numberToString(num) {
+//   // Return a string of the number here!
+//   return num.toString();
+// }
+
+const numberToString = (num) => num.toString();
+
+console.log(numberToString(666));
