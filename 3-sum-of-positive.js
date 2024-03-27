21a757d6b0b44d51e12e54cf8c5aa7048b22ed1d
Abaljerind <abaljerinda@gmail.com>
2024-03-27 08:48:07 +0700

    update old function 3/366

--- 3-sum-of-positive.js
+++ 3-sum-of-positive.js
@@ -2,22 +2,22 @@
 // 3/366
 // https://www.codewars.com/kata/5715eaedb436cf5606000381/train/javascript
 
-function positiveSum(arr) {
-  // buat variable result untuk hasil penjumlahan
-  //   let result = 0;
-  //   //   filter param arr agar yang ditampilkan hanya yang positif
-  //   const filteredArray = arr.filter(checkPositive);
-  //   //   buat function untuk mengecek number bernilai positif, kembalikan / tampilkan number yang lebih dari atau sama dengan 0
-  //   function checkPositive(number) {
-  //     return number >= 0;
-  //   }
-  //   // buat perulangan berdasarkan variable filteredArray
-  //   for (i = 0; i < filteredArray.length; i++) {
-  //     result += filteredArray[i];
-  //   }
-  //   //   menampilkan variable result
-  //   return result;
-}
+// function positiveSum(arr) {
+//   buat variable result untuk hasil penjumlahan
+//     let result = 0;
+//     //   filter param arr agar yang ditampilkan hanya yang positif
+//     const filteredArray = arr.filter(checkPositive);
+//     //   buat function untuk mengecek number bernilai positif, kembalikan / tampilkan number yang lebih dari atau sama dengan 0
+//     function checkPositive(number) {
+//       return number >= 0;
+//     }
+//     // buat perulangan berdasarkan variable filteredArray
+//     for (i = 0; i < filteredArray.length; i++) {
+//       result += filteredArray[i];
+//     }
+//     //   menampilkan variable result
+//     return result;
+// }
 
 function positiveSum(arr) {
   let result = 0;
