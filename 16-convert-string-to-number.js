17ce55085eceff0605b03833e38f689ca1445dfc
Abaljerind <abaljerinda@gmail.com>
2024-04-01 05:58:50 +0700

    day 16/366

---
+++ 16-convert-string-to-number.js
@@ -0,0 +1,9 @@
+// Codewars Challenge 2024
+// 16/366
+// https://www.codewars.com/kata/544675c6f971f7399a000e79/train/javascript
+
+function stringToNumber(str) {
+  return parseInt(str);
+}
+
+console.log(stringToNumber("121"));
