3f20a73f3cf2c5c4159ceafbe078f6ca41628c15
Abaljerind <abaljerinda@gmail.com>
2024-04-01 07:01:39 +0700

    day 18/366

---
+++ 18-keep-hydrated.js
@@ -0,0 +1,10 @@
+// Codewars Challenge 2024
+// 18/366
+// https://www.codewars.com/kata/582cb0224e56e068d800003c/train/javascript
+
+// 0.5 per jam
+function litres(time) {
+  return Math.floor(0.5 * time);
+}
+
+console.log(litres(11.8));
