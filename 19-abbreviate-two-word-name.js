5f6f3dd9cfe3ad6a5ba4ce7fe07d972c72a064a5
Abaljerind <abaljerinda@gmail.com>
2024-04-03 06:44:04 +0700

    add alternative codes to day 19/366

--- 19-abbreviate-two-word-name.js
+++ 19-abbreviate-two-word-name.js
@@ -3,11 +3,17 @@
 // https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/javascript
 
 function abbrevName(name) {
-  let nameSplit = name.split(" ");
-  let firstName = nameSplit[0];
-  let lastName = nameSplit[1];
+  //   let nameSplit = name.split(" ");
+  //   let firstName = nameSplit[0];
+  //   let lastName = nameSplit[1];
 
-  return firstName[0].toUpperCase() + "." + lastName[0].toUpperCase();
+  //   return nameSplit[0][0] + "." + nameSplit[1][0];
+  //   return firstName[0].toUpperCase() + "." + lastName[0].toUpperCase();
+
+  return name
+    .split(" ")
+    .map((i) => i[0])
+    .join(".");
 }
 
 console.log(abbrevName("Patrick Feenan"));
