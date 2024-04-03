e8bc8f9b97f7f65aa46d429a39271f1b513bc14a
Abaljerind <abaljerinda@gmail.com>
2024-04-03 07:07:51 +0700

    day 21/366

---
+++ 21-returning-strings.js
@@ -0,0 +1,7 @@
+// Codewars Challenge 2024
+// 21/366
+// https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+
+const greet = (name) => `Hello, ${name} how are you doing today?`;
+
+console.log(greet("naruto"));
