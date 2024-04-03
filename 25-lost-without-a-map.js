43e97b985b2e9117f024eecda9bf2f1b6eb892f9
Abaljerind <abaljerinda@gmail.com>
2024-04-04 06:53:52 +0700

    day 25/366

---
+++ 25-lost-without-a-map.js
@@ -0,0 +1,11 @@
+// Codewars Challenge 2024
+// 25/366
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
+
+function maps(x) {
+  return x.map((item) => {
+    return item * 2;
+  });
+}
+
+console.log(maps([4, 1, 1, 1, 4]));
