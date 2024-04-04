b6e79b2d205a13662a8ccf868158a3b1e5698fb3
Abaljerind <abaljerinda@gmail.com>
2024-04-04 07:01:49 +0700

    day 27/366

---
+++ 27-convert-boolean-to-string.js
@@ -0,0 +1,9 @@
+// Codewars Challenge 2024
+// 27/366
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+
+function booleanToString(b) {
+  return b.toString();
+}
+
+console.log(booleanToString(true));
