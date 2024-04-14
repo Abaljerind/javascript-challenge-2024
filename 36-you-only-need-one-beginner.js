34d17fdd334334e522b7163ac1eebf29f2a7c5c7
Abaljerind <abaljerinda@gmail.com>
2024-04-14 17:53:40 +0700

    day 36/366

---
+++ 36-you-only-need-one-beginner.js
@@ -0,0 +1,11 @@
+// Codewars Challenge 2024
+// 36/366
+// https://www.codewars.com/kata/57cc975ed542d3148f00015b/train/javascript
+
+function check(a, x) {
+  return a.includes(x);
+}
+
+const arrayData = ["t", "e", "s", "t"];
+const valueData = "e";
+console.log(check(arrayData, valueData));
