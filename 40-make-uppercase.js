d4a34261930a630c46a17d5430ac8c270f106e92
Abaljerind <abaljerinda@gmail.com>
2024-04-17 09:41:07 +0700

    day 40/366

---
+++ 40-make-uppercase.js
@@ -0,0 +1,11 @@
+// Codewars Challenge 2024
+// 40/366
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+
+// function makeUpperCase(str) {
+//   return str.toUpperCase();
+// }
+
+const makeUpperCase = (str) => str.toUpperCase();
+
+console.log(makeUpperCase("hello"));
