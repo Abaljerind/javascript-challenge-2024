859c89fe2ee34a0e67c6a98a6e2b40242be1b8bb
Abaljerind <abaljerinda@gmail.com>
2024-04-19 14:01:23 +0700

    day 42/366

---
+++ 42-sentence-smash.js
@@ -0,0 +1,9 @@
+// Codewars Challenge 2024
+// 42/366
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript
+
+function smash(words) {
+  return words.join(" ");
+}
+
+console.log(smash(["hello", "world", "this", "is", "great"]));
