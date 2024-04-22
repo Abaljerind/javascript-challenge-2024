98d946ffbe15a5ef521e46de6b8f85a3b8feb018
Abaljerind <abaljerinda@gmail.com>
2024-04-22 09:43:06 +0700

    day 53/366

---
+++ 53-convert-a-string-to-an-array.js
@@ -0,0 +1,9 @@
+// Codewars Challenge 2024
+// 53/366
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/javascript
+
+function stringToArray(string) {
+  return string.split(" ");
+}
+
+console.log(stringToArray("I love arrays they are my favorite"));
