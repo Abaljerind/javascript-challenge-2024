60952bff2fe3657324bc5834ff091bc3b6e52135
Abaljerind <abaljerinda@gmail.com>
2024-04-22 07:15:52 +0700

    day 49/366

---
+++ 49-jennys-secret-messege.js
@@ -0,0 +1,11 @@
+// Codewars Challenge 2024
+// 49/366
+// https://www.codewars.com/kata/55225023e1be1ec8bc000390/train/javascript
+
+function greet(name) {
+  return name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
+}
+
+console.log(greet("jim"));
+console.log(greet("jane"));
+console.log(greet("Johnny"));
