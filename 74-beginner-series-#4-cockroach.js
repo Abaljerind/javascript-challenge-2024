f930c25fdaf7d84284f146b0e19e5dd441f000b0
AbalJerind <abaljerinda@gmail.com>
2024-05-04 10:28:20 +0700

    day 74/366

---
+++ 74-beginner-series-#4-cockroach.js
@@ -0,0 +1,11 @@
+// Codewars Challenge 2024
+// 74/366
+// https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/javascript
+
+function cockroachSpeed(s) {
+  return parseInt(Math.floor(27.778 * s));
+}
+
+console.log(cockroachSpeed(1.08));
+console.log(cockroachSpeed(1.09));
+console.log(cockroachSpeed(0));
