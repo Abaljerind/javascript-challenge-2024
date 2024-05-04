c0bacc1124e17931786a3c68dcaa959b6744ca3f
AbalJerind <abaljerinda@gmail.com>
2024-05-04 11:04:46 +0700

    day 76/366

---
+++ 76-grasshopper-check-for-factor.js
@@ -0,0 +1,13 @@
+// Codewars Challenge 2024
+// 76/366
+// https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/javascript
+
+function checkForFactor(base, factor) {
+  return base % factor == 0;
+}
+
+console.log(checkForFactor(10, 2));
+console.log(checkForFactor(63, 7));
+console.log(checkForFactor(2450, 5));
+console.log(checkForFactor(9, 2));
+console.log(checkForFactor(653, 7));
