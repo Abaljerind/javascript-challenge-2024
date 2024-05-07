c7582e86014ee8e79be1c409c449bb7c7a9ca03c
Abaljerind <abaljerinda@gmail.com>
2024-05-07 09:56:11 +0700

    day 77/366

---
+++ 77-twice-as-old.js
@@ -0,0 +1,12 @@
+// Codewars Challenge 2024
+// 77/366
+// https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - sonYearsOld * 2);
+}
+
+console.log(twiceAsOld(36, 7));
+console.log(twiceAsOld(55, 30));
+console.log(twiceAsOld(42, 21));
+console.log(twiceAsOld(22, 1));
