65e53b4a61c1af263206fb25f406571888cbbb9f
AbalJerind <abaljerinda@gmail.com>
2024-05-09 08:47:42 +0700

    day 81/366

---
+++ 81-volume-of-a-cuboid.js
@@ -0,0 +1,12 @@
+// Codewars Challenge 2024
+// 81/366
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/javascript
+
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
+  }
+}
+
+console.log(Kata.getVolumeOfCuboid(1, 2, 2));
+console.log(Kata.getVolumeOfCuboid(6, 2, 5));
