633f0d29369f9b71e6446fab649ff443ad80e023
Abaljerind <abaljerinda@gmail.com>
2024-05-10 10:13:47 +0700

    day 84/366

---
+++ 84-parse-nice-int-from-char-problem.js
@@ -0,0 +1,13 @@
+// Codewars Challenge 2024
+// 84/366
+// https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1/train/javascript
+
+function getAge(inputString) {
+  // return the girl's correct age as an integer. Happy coding :)
+  return +inputString[0];
+  //   return parseInt(inputString);
+}
+
+console.log(getAge("4 years old"));
+console.log(getAge("8 years old"));
+console.log(getAge("6 years old"));
