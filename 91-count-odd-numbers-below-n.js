4679ac98d9f5d6b8ef04ddc35ce760eecf87beff
Abaljerind <abaljerinda@gmail.com>
2024-05-16 10:03:07 +0700

    day 91/366

---
+++ 91-count-odd-numbers-below-n.js
@@ -0,0 +1,11 @@
+// Codewars Challenge 2024
+// 91/366
+// https://www.codewars.com/kata/59342039eb450e39970000a6/train/javascript
+
+function oddCount(n) {
+  return Math.floor(n / 2);
+}
+
+console.log(oddCount(7));
+console.log(oddCount(15));
+console.log(oddCount(15023));
