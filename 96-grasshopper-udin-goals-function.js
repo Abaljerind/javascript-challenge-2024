b6f432fee910c7e0ae475fb89a04213efe19255c
Abaljerind <abaljerinda@gmail.com>
2024-05-17 10:16:35 +0700

    day 96/366

---
+++ 96-grasshopper-udin-goals-function.js
@@ -0,0 +1,10 @@
+// Codewars Challenge 2024
+// 96/366
+// https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/javascript
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+console.log(goals(0, 0, 0));
+console.log(goals(43, 10, 5));
