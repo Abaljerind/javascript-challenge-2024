6ae6493c0e8be411c130c728ce160abd62da80a8
Abaljerind <abaljerinda@gmail.com>
2024-05-18 13:15:49 +0700

    day 99/366

---
+++ 99-grasshopper-debug-sayHello.js
@@ -0,0 +1,12 @@
+// Codewars Challenge 2024
+// 99/366
+// https://www.codewars.com/kata/5625618b1fe21ab49f00001f/train/javascript
+
+function sayHello(name) {
+  return `Hello, ${name}`;
+}
+
+console.log(sayHello("Mr. Spock"));
+console.log(sayHello("Captain Kirk"));
+console.log(sayHello("Liutenant Uhura"));
+console.log(sayHello("Dr. McCoy"));
