b4bc7a4a031fc70c1ceb47f0815f28c06110462b
Abaljerind <abaljerinda@gmail.com>
2024-07-01 11:19:37 +0700

    add alternative code

--- 169-the-coupon-code.js
+++ 169-the-coupon-code.js
@@ -2,28 +2,35 @@
 // 169/366
 // https://www.codewars.com/kata/539de388a540db7fec000642/train/javascript
 
-function parseDate(dateString) {
-  // Membuat objek Date dari string
-  let date = new Date(dateString);
+// function parseDate(dateString) {
+//   // Membuat objek Date dari string
+//   let date = new Date(dateString);
 
-  // Mengatur waktu ke tengah malam (00:00:00)
-  date.setHours(0, 0, 0, 0);
+//   // Mengatur waktu ke tengah malam (00:00:00)
+//   date.setHours(0, 0, 0, 0);
 
-  return date;
-}
+//   return date;
+// }
 
-console.log(parseDate("September 5, 2014"));
-console.log(parseDate("October 10, 2020"));
+// console.log(parseDate("September 5, 2014"));
+// console.log(parseDate("October 10, 2020"));
+
+// function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+//   //   membuat object date dari currentDate dan expirationDate
+//   let current = parseDate(currentDate);
+//   let expiration = parseDate(expirationDate);
+
+//   // menambahkan 1 hari ke tanggal kadaluwarsa
+//   expiration.setDate(expiration.getDate() + 1);
+
+//   return enteredCode === correctCode && current < expiration;
+// }
 
 function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
-  //   membuat object date dari currentDate dan expirationDate
-  let current = parseDate(currentDate);
-  let expiration = parseDate(expirationDate);
-
-  // menambahkan 1 hari ke tanggal kadaluwarsa
-  expiration.setDate(expiration.getDate() + 1);
-
-  return enteredCode === correctCode && current < expiration;
+  return (
+    enteredCode === correctCode &&
+    Date.parse(currentDate) <= Date.parse(expirationDate)
+  );
 }
 
 console.log(checkCoupon("123", "123", "September 5, 2014", "October 1, 2014"));
