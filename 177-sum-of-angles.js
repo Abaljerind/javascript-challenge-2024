8991fe5148f172aa2cbedbfaeb216d605d5d054b
Abaljerind <abaljerinda@gmail.com>
2024-07-05 08:30:26 +0700

    day 177/366

---
+++ 177-sum-of-angles.js
@@ -0,0 +1,10 @@
+// Codewars Challenge 2024
+// 177/366
+// https://www.codewars.com/kata/5a03b3f6a1c9040084001765/train/javascript
+
+function angle(n) {
+  return (n - 2) * 180;
+}
+
+console.log(angle(3));
+console.log(angle(4));
