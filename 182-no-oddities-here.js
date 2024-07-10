14a9f81066a7e69b672147c43f135e8778c2ad9c
Abaljerind <abaljerinda@gmail.com>
2024-07-10 10:28:32 +0700

    day 182/366

---
+++ 182-no-oddities-here.js
@@ -0,0 +1,8 @@
+// Codewars Challenge 2024
+// 182/366
+// https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/train/javascript
+
+const noOdds = (values) => values.filter((item) => item % 2 === 0);
+
+console.log(noOdds([0, 1]));
+console.log(noOdds([0, 1, 2, 3]));
