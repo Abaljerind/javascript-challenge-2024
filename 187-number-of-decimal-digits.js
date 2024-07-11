be5435fa5f48711a2685a7c1dc040227f61217ba
Abaljerind <abaljerinda@gmail.com>
2024-07-11 10:01:03 +0700

    day 187/366

---
+++ 187-number-of-decimal-digits.js
@@ -0,0 +1,11 @@
+// Codewars Challenge 2024
+// 187/366
+// https://www.codewars.com/kata/58fa273ca6d84c158e000052/train/javascript
+
+function digits(n) {
+  return n.toString().length;
+}
+
+console.log(digits(0));
+console.log(digits(56));
+console.log(digits(12345));
