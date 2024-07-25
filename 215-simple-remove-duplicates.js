6db772308880f053e9a2c8302fae53cb9ca068ab
Abaljerind <abaljerinda@gmail.com>
2024-07-25 07:40:58 +0700

    add alternative way

--- 215-simple-remove-duplicates.js
+++ 215-simple-remove-duplicates.js
@@ -3,14 +3,14 @@
 // https://www.codewars.com/kata/5ba38ba180824a86850000f7/train/javascript
 
 function solve(arr) {
-  //   return [...new Set(arr)];
-  const result = [];
-  for (let i = arr.length - 1; i >= 0; i--) {
-    if (!result.includes(arr[i])) {
-      result.unshift(arr[i]);
-    }
-  }
-  return result;
+  return [...new Set(arr.reverse())].reverse();
+  //   const result = [];
+  //   for (let i = arr.length - 1; i >= 0; i--) {
+  //     if (!result.includes(arr[i])) {
+  //       result.unshift(arr[i]);
+  //     }
+  //   }
+  //   return result;
 }
 
 console.log(solve([3, 4, 4, 3, 6, 3]));
