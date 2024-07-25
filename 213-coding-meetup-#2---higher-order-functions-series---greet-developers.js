cfabee8237eb078f213d3bc7a67c867488b18f93
Abaljerind <abaljerinda@gmail.com>
2024-07-25 07:25:05 +0700

    day 213/366

---
+++ 213-coding-meetup-#2---higher-order-functions-series---greet-developers.js
@@ -0,0 +1,38 @@
+// Codewars Challenge 2024
+// 212/366
+// https://www.codewars.com/kata/58279e13c983ca4a2a00002a/train/javascript
+
+function greetDevelopers(list) {
+  return list.map((dev) => {
+    dev.greeting = `Hi ${dev.firstName}, what do you like the most about ${dev.language}?`;
+    return dev;
+  });
+}
+
+const list1 = [
+  {
+    firstName: "Sofia",
+    lastName: "I.",
+    country: "Argentina",
+    continent: "Americas",
+    age: 35,
+    language: "Java",
+  },
+  {
+    firstName: "Lukas",
+    lastName: "X.",
+    country: "Croatia",
+    continent: "Europe",
+    age: 35,
+    language: "Python",
+  },
+  {
+    firstName: "Madison",
+    lastName: "U.",
+    country: "United States",
+    continent: "Americas",
+    age: 32,
+    language: "Ruby",
+  },
+];
+console.log(greetDevelopers(list1));
