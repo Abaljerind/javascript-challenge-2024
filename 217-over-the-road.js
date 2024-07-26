b3189c744a8e9f618bc7ee639742e7554731c60c
AbalJerind <abaljerinda@gmail.com>
2024-07-26 09:07:30 +0700

    day 217/366

---
+++ 217-over-the-road.js
@@ -0,0 +1,11 @@
+// Codewars Challenge 2024
+// 217/366
+// https://www.codewars.com/kata/5f0ed36164f2bc00283aed07/train/javascript
+
+function overTheRoad(address, n) {
+  return 2 * n + 1 - address;
+}
+
+console.log(overTheRoad(1, 3));
+console.log(overTheRoad(3, 3));
+console.log(overTheRoad(2, 3));
