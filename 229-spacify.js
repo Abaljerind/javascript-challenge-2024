b9ef63a9ede46fdecb495e7768ac75ca60fb22de
Abaljerind <abaljerinda@gmail.com>
2024-08-07 11:34:32 +0700

    day 229/366

---
+++ 229-spacify.js
@@ -0,0 +1,10 @@
+// Codewars Challenge 2024
+// 229/366
+// https://www.codewars.com/kata/57f8ee485cae443c4d000127/train/javascript
+
+function spacify(str) {
+  return str.split("").join(" ");
+}
+
+console.log(spacify("hello world"));
+console.log(spacify("12345"));
