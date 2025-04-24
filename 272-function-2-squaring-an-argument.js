3d6df3a1b040507376282806c1c430e96d4f8117
Abaljerind <abaljerinda@gmail.com>
2025-04-24 07:21:03 +0700

    Day 272/366

---
+++ 272-function-2-squaring-an-argument.js
@@ -0,0 +1,12 @@
+// Codewars Challenge 2024
+// 272/366
+// https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+
+// TODO: Now you have to write a function that takes an argument and returns the square of it.
+
+// Write the "square"-function here
+function square(num) {
+  return num ** 2;
+}
+
+console.log(square(3));
