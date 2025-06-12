35842f5c869a1940183e8ca0f77b4a4b6276489c
Abaljerind <abaljerinda@gmail.com>
2025-06-12 09:44:38 +0700

    Day 285/366

---
+++ 285-fixme-get-full-name.js
@@ -0,0 +1,33 @@
+// Codewars Challenge 2024
+// 285/366
+// https://www.codewars.com/kata/597c684822bc9388f600010f/train/javascript
+
+/*
+
+The code provided is supposed return a person's Full Name given their first and last names.
+
+But it's not working properly.
+
+Notes
+The first and/or last names are never null, but may be empty.
+
+Task
+Fix the bug so we can all go home early.
+
+*/
+
+class Dinglemouse {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  getFullName() {
+    return (this.firstName + " " + this.lastName).trim();
+  }
+}
+
+const dinglemouse = new Dinglemouse("Naruto", "Uzumaki");
+const dinglemouse1 = new Dinglemouse("", "");
+console.log(dinglemouse.getFullName());
+console.log(dinglemouse1.getFullName());
