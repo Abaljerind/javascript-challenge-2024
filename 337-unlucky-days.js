2c527a6e3cb06aee2b57d7fb0353b0ef8003a784
Abaljerind <abaljerinda@gmail.com>
2025-08-08 10:10:45 +0700

    Day 337/366

---
+++ 337-unlucky-days.js
@@ -0,0 +1,35 @@
+// Codewars Challenge 2024
+// 337/366
+// https://www.codewars.com/kata/56eb0be52caf798c630013c0/train/javascript
+
+/*
+Friday 13th or Black Friday is considered as unlucky day. Calculate how many unlucky days are in the given year.
+
+Find the number of Friday 13th in the given year.
+
+Input: Year in Gregorian calendar as integer.
+
+Output: Number of Black Fridays in the year as an integer.
+
+Examples:
+
+unluckyDays(2015) == 3
+unluckyDays(1986) == 1
+
+*/
+
+function unluckyDays(year) {
+  let count = 0;
+
+  for (let month = 0; month < 12; month++) {
+    const date = new Date(year, month, 13);
+    if (date.getDay() === 5) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(unluckyDays(2819));
+console.log(unluckyDays(2792));
+console.log(unluckyDays(1909));
